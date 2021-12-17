b2755b93212af38ed5e5e15f9b3329699039abff
nev3rofficial <mayafmayaf@icloud.com>
2021-12-16 17:15:14 -0800

    proofreading and clarification for controls

--- src/main/java/com/pigmice/frc/robot/Controls.java
+++ src/main/java/com/pigmice/frc/robot/Controls.java
@@ -1,10 +1,9 @@
 package com.pigmice.frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+// import edu.wpi.first.wpilibj.XboxController.Button;
+// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-import com.pigmice.frc.robot.commands.subroutines.TurnToAngle;
 import com.pigmice.frc.robot.subsystems.impl.Drivetrain;
 
 import edu.wpi.first.wpilibj.XboxController;
@@ -30,18 +29,19 @@ public class Controls {
     }
 
     public double turnSpeed() {
+        double epsilon = 0.1;
         double left = driver.getX(Hand.kLeft);
+        left = Math.abs(left) > epsilon ? left : 0d;
         double right = driver.getX(Hand.kRight);
-        return (Math.abs(right) > 0.1 ? right / 4 : left) / 4;
+        right = Math.abs(right) > epsilon ? right : 0d;
+        return (left != 0 ? left / 4 : right) / 4;
     }
 
     public double driveSpeed() {
         double value = driver.getTriggerAxis(Hand.kRight) - driver.getTriggerAxis(Hand.kLeft);
+        boolean lowSpeed = driver.getAButton();
+        value *= lowSpeed ? 0.375 : 1;
         return value / (Drivetrain.getInstance().isBoosting() ? 1 : 4);
-    }
-
-    public boolean getYbutton() {
-        return driver.getYButton();
     }
 
     /*
